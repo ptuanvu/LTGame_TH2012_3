97532538baa6c693e78306e66fbf7a5ee51a9425
ptuanvu <clan_hunter@quangbinh.org>
2015-10-02 23:30:04 +0700

    Sync bullet position

--- Chapter02_CreativeMenu/Chapter02_CreativeMenu/Bullet.cs
+++ Chapter02_CreativeMenu/Chapter02_CreativeMenu/Bullet.cs
@@ -18,6 +18,7 @@ namespace Chapter02_CreativeMenu
         private static int OVER3 = 6;
         private GameTime timeStart;
         private int mili = 0;
+        private int second = 0;
         private List<Texture2D> bullets = new List<Texture2D>();
         private Ship ship;
         private int state = 0;
@@ -31,7 +32,10 @@ namespace Chapter02_CreativeMenu
         private int degrees = 0;
         private Vector2 spriteVelocity;
 
-        private const float tangentialVelocity = 5f;
+        //Bullet Change
+        
+
+        private const float tangentialVelocity = 10f;
         private float friction = 0.1f;
 
         public override string ObjectName()
@@ -41,6 +45,18 @@ namespace Chapter02_CreativeMenu
 
         public override void Update(GameTime gameTime)
         {
+            if (state >= 6)
+            {
+                return;
+            }
+
+            if (gameTime.TotalGameTime.Seconds - second > 1 && state < 6)
+            {
+                state++;
+            }
+            
+            UpdateBulletShape(this.state);
+
             position = spriteVelocity + position;
 
             spriteVelocity.X = (float)Math.Sin(rotation) * tangentialVelocity;
@@ -56,27 +72,50 @@ namespace Chapter02_CreativeMenu
             }
         }
 
+        private void UpdateBulletShape(int state)
+        {
+            if (state < 4)
+                state++;
+        }
+
+
         public override void Draw(GameTime gameTime, Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
         {
+            if (state > 7)
+            {
+                state = 6;
+            }
             spriteBatch.Draw(bullets[state], new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y), null, color, rotation, Vector2.Zero, SpriteEffects.None, 0);
+        }
+
+        private Vector2 SyncBullet(Vector2 p)
+        {
+            Vector2 spriteVelocity2;
+            float tangentialVelocity2 = 19f;
+
+            spriteVelocity2.Y = (float)Math.Sin(rotation) * tangentialVelocity2;
+            spriteVelocity2.X = (float)Math.Cos(rotation) * tangentialVelocity2;
+
+            p = spriteVelocity2 + p;
+            return p;
         }
 
         public Bullet(Vector2 position, float p, Color color, GameTime gameTime)
         {
             // TODO: Complete member initialization
             this.bullets = Textures.bullets;
-            this.position = position;
-            this.position.X += 19;
             this.rotation = p;
             this.color = color;
             this.size.X = 15;
             this.size.Y = 35;
-            mili = gameTime.TotalGameTime.Seconds;
+            this.position = SyncBullet(position);
+            second = gameTime.TotalGameTime.Seconds;
+            mili = gameTime.TotalGameTime.Milliseconds;
         }
 
         public bool IsPosibleToAddNewBullet(Microsoft.Xna.Framework.GameTime gameTime)
         {
-            if (Math.Abs(gameTime.TotalGameTime.Seconds - mili) > 0)
+            if (Math.Abs(gameTime.TotalGameTime.Seconds - second) > 0 || (Math.Abs(gameTime.TotalGameTime.Seconds - second) == 0 && Math.Abs(gameTime.TotalGameTime.Milliseconds - mili) > 500))
             {
                 return true;
             }
