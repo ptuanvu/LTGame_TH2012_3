e1b269e5e4c87083c120b8905bc479c5742f1917
ptuanvu <clan_hunter@quangbinh.org>
2015-11-07 02:25:53 +0700

    Going Beyond done Tut1 + 2 + 3

--- GoingBeyond/GoingBeyond/Game1.cs
+++ GoingBeyond/GoingBeyond/Game1.cs
@@ -28,8 +28,16 @@ namespace GoingBeyond
         float aspectRatio;
 
         // Position of model
-        Vector3 position = Vector3.Zero;
+        Vector3 modelPosition = Vector3.Zero;
         float modelRotation = 0.0f;
+
+        // Set the velocity of the model, applied each frame to the model's position.
+        Vector3 modelVelocity = Vector3.Zero;
+
+        //Set the sound effects to use
+        SoundEffect soundEngine;
+        SoundEffectInstance soundEngineInstance;
+        SoundEffect soundHyperspaceActivation;
 
         // Set position of the camera in world space, for our view matrix
         Vector3 cameraPosition = new Vector3(0.0f, 50.0f, 5000.0f);
@@ -68,6 +76,11 @@ namespace GoingBeyond
             //Load the ship
             myModel = Content.Load<Model>(@"Models\\p1_wedge");
 
+            soundEngine = Content.Load<SoundEffect>(@"Audio\\Waves\\engine_2");
+            soundEngineInstance = soundEngine.CreateInstance();
+
+            soundHyperspaceActivation = Content.Load<SoundEffect>(@"Audio\\Waves\\hyperspace_activate");
+
             //Load Aspect Ratio
             aspectRatio = GraphicsDevice.Viewport.AspectRatio;
         }
@@ -92,9 +105,92 @@ namespace GoingBeyond
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
-            modelRotation += (float)gameTime.ElapsedGameTime.TotalMilliseconds * MathHelper.ToRadians(0.1f);
+            //modelRotation += (float)gameTime.ElapsedGameTime.TotalMilliseconds * MathHelper.ToRadians(0.1f);
 
+            // Get some input
+            UpdateInput();
+
+            // Add velocity to the current position
+            modelPosition += modelVelocity;
+
+            // Bleed off velocity over time
+            modelVelocity *= 0.95f;
             base.Update(gameTime);
+        }
+
+        protected void UpdateInput()
+        {
+            // Get the game pad state.
+            GamePadState currentState = GamePad.GetState(PlayerIndex.One);
+            KeyboardState currentKeyState = Keyboard.GetState();
+
+            if (true)
+            {
+                
+
+                // Rotate the model using the left thumbstick, and scale it down.
+                modelRotation -= currentState.ThumbSticks.Left.X * 0.10f;
+
+                // Create some velocity if the right trigger is down.
+                Vector3 modelVelocityAdd = Vector3.Zero;
+
+                if (currentKeyState.IsKeyDown(Keys.A))
+                    modelRotation += 0.10f;
+                else if (currentKeyState.IsKeyDown(Keys.D))
+                    modelRotation -= 0.10f;
+                
+                
+
+                // Find out what direction we should be thrusting, using rotation.
+                modelVelocityAdd.X = -(float)Math.Sin(modelRotation);
+                modelVelocityAdd.Z = -(float)Math.Cos(modelRotation);
+
+
+                if (currentKeyState.IsKeyDown(Keys.W))
+                {
+                    
+                    modelVelocityAdd *= 2;
+                } 
+                    
+
+                // Now scale our direction by how hard the trigger is down.
+                // modelVelocityAdd *= currentState.Triggers.Right;
+
+                // Finally, add this vector to our velocity.
+                modelVelocity += modelVelocityAdd;
+                Window.Title = modelVelocity.ToString();
+
+                GamePad.SetVibration(PlayerIndex.One, currentState.Triggers.Right,
+                    currentState.Triggers.Right);
+
+                //Play engine sound only when the engine is on.
+                if (currentState.Triggers.Right > 0)
+                {
+
+                    if (soundEngineInstance.State == SoundState.Stopped)
+                    {
+                        soundEngineInstance.Volume = 0.75f;
+                        soundEngineInstance.IsLooped = true;
+                        soundEngineInstance.Play();
+                    }
+                    else
+                        soundEngineInstance.Resume();
+                }
+                else if (currentState.Triggers.Right == 0)
+                {
+                    if (soundEngineInstance.State == SoundState.Playing)
+                        soundEngineInstance.Pause();
+                }
+
+                // In case you get lost, press A to warp back to the center.
+                if (currentState.Buttons.A == ButtonState.Pressed || currentKeyState.IsKeyDown(Keys.Enter))
+                {
+                    modelPosition = Vector3.Zero;
+                    modelVelocity = Vector3.Zero;
+                    modelRotation = 0.0f;
+                    soundHyperspaceActivation.Play();
+                }
+            }
         }
 
         /// <summary>
@@ -115,7 +211,7 @@ namespace GoingBeyond
                     effect.EnableDefaultLighting();
                     effect.World = transform[mesh.ParentBone.Index] *
                         Matrix.CreateRotationY(modelRotation) *
-                        Matrix.CreateTranslation(position);
+                        Matrix.CreateTranslation(modelPosition);
 
                     effect.View = Matrix.CreateLookAt(cameraPosition, Vector3.Zero, Vector3.Up);
                     effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), aspectRatio
